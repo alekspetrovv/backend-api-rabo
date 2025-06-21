0cc55a3c08bac54ddb31743b4a9b50dfcddf3e0d
Aleks Petrov <a.petrov@spinnov.com>
2025-06-21 12:46:43 +0200

    chore: add commenst

--- data/src/main/java/nl/rabobank/mongo/document/account/PaymentAccountDocument.java
+++ data/src/main/java/nl/rabobank/mongo/document/account/PaymentAccountDocument.java
@@ -3,5 +3,6 @@ package nl.rabobank.mongo.document.account;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+// useful when needs to be extended in the future
 public class PaymentAccountDocument extends AccountDocument {
 }
